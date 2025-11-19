83346bef40312590c9337828d7a9f2647eef8f54
Tomer Davidson <davidtom@post.bgu.ac.il>
2025-11-19 14:49:11 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
-import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: 'CourseWise',
